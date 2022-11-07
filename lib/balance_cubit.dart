96c4d095eafc08fd21fb3d5daaa64b432851cad0
Oskar Zajac <oskarzajac@Oskars-Mac-mini.local>
2022-11-07 21:53:11 +0100

    storing balance in cubit

--- lib/balance_cubit.dart
+++ lib/balance_cubit.dart
@@ -3,23 +3,25 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BalanceCubit extends Cubit<BalanceState> {
-  BalanceCubit() : super(FetchedBalance());
+  BalanceCubit() : super(NoBalance());
 
   FirebaseAuth auth = FirebaseAuth.instance;
   final DatabaseReference _dataRef = FirebaseDatabase.instance.ref();
 
-  Future<double> getBalance() async {
+  double balance = -1;
+
+  getBalance() async {
     emit(FetchingInProgress());
 
     try {
       DataSnapshot response = await _dataRef.child('Balance').get();
       final data = response.value as dynamic;
-      return data.toDouble();
+      balance = data.toDouble();
     } catch (err, st) {
       print('Error: $err, $st');
     }
     emit(FetchedBalance());
-    return -1.toDouble();
+    balance = -1.toDouble();
   }
 }
 
@@ -28,3 +30,6 @@ abstract class BalanceState {}
 class FetchedBalance extends BalanceState {}
 
 class FetchingInProgress extends BalanceState {}
+
+class NoBalance extends BalanceState {}
+ 
