011036e3bdfacf5482a2928f1b6fe5703cc23e02
Oskar Zajac <oskarzajac@Oskars-Mac-mini.local>
2022-11-08 20:47:52 +0100

    moved balance to state and displaying balance on home_screen

--- lib/balance_cubit.dart
+++ lib/balance_cubit.dart
@@ -8,28 +8,27 @@ class BalanceCubit extends Cubit<BalanceState> {
   FirebaseAuth auth = FirebaseAuth.instance;
   final DatabaseReference _dataRef = FirebaseDatabase.instance.ref();
 
-  double balance = -1;
-
   getBalance() async {
     emit(FetchingInProgress());
 
     try {
       DataSnapshot response = await _dataRef.child('Balance').get();
       final data = response.value as dynamic;
-      balance = data.toDouble();
+      emit(FetchedBalance(balance: data.toDouble()));
     } catch (err, st) {
       print('Error: $err, $st');
     }
-    emit(FetchedBalance());
-    balance = -1.toDouble();
   }
 }
 
 abstract class BalanceState {}
 
-class FetchedBalance extends BalanceState {}
+class FetchedBalance extends BalanceState {
+  FetchedBalance({required this.balance});
+
+  final double balance;
+}
 
 class FetchingInProgress extends BalanceState {}
 
 class NoBalance extends BalanceState {}
- 
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pcyb/auth_cubit.dart';
-import 'package:provider/provider.dart';
+import 'package:pcyb/balance_cubit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: const Center(
-          child: Text('Your balance is \$100'),
+  Widget build(BuildContext context) {
+    context.read<BalanceCubit>().getBalance();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: BlocBuilder<BalanceCubit, BalanceState>(
+          builder: (context, state) {
+            if (state is FetchedBalance) {
+              return Text('Current balance: ${state.balance}\$');
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: context.read<AuthCubit>().logOut,
-          child: const Icon(Icons.logout),
-        ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: context.read<AuthCubit>().logOut,
+        child: const Icon(Icons.logout),
+      ),
+    );
+  }
 }
